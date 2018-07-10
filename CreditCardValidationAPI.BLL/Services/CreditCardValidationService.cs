ad7654816bb12ad54cd1b9eb82f27cdf8ff2192d
Sarawut Positwinyu <sarawut.positwinyu@gmail.com>
2018-07-10 23:18:03 +0700

    Refactor: refactor ValidateCreditCardType to reduce duplication

--- CreditCardValidationAPI.BLL/Services/CreditCardValidationService.cs
+++ CreditCardValidationAPI.BLL/Services/CreditCardValidationService.cs
@@ -1,33 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 using CreditCardValidationAPI.BLL.Domain;
 
 namespace CreditCardValidationAPI.BLL.Services
 {
     public class CreditCardValidationService : ICreditCardValidationServices
-    {       
+    {
+        private class CardTypeRule
+        {
+            public const short DEFAULT_LENGTH = 16;
+            public short StartNumber;
+            public short Length = DEFAULT_LENGTH;
+            public CardType CardType;
+        }
+
+        private List<CardTypeRule> _cardTypeRules;
+
+        public CreditCardValidationService()
+        {
+            this._cardTypeRules = new List<CardTypeRule>()
+            {
+                new CardTypeRule(){ StartNumber = 4, CardType = CardType.Visa},
+                new CardTypeRule(){ StartNumber = 5, CardType = CardType.MasterCard},
+                new CardTypeRule(){ StartNumber = 3, CardType = CardType.JCB},
+                new CardTypeRule(){ StartNumber = 3, CardType = CardType.Amex, Length = 15}
+            };
+        }
+
         public CardType ValidateCreditCardType(string creditCardNumber)
         {
-            if (creditCardNumber.StartsWith("4") && creditCardNumber.Length == 16)
-            {
-                return CardType.Visa;
-            }
-            if (creditCardNumber.StartsWith("5") && creditCardNumber.Length == 16)
-            {
-                return CardType.MasterCard;
-            }
-            if (creditCardNumber.StartsWith("3"))
-            {
-                if(creditCardNumber.Length == 15) {
-                    return CardType.Amex;
-                }
-                if (creditCardNumber.Length == 16)
-                {
-                    return CardType.JCB;
-                }
-            }
-            return CardType.Unknown;
+            var matchedRule = this._cardTypeRules.FirstOrDefault(rule=> creditCardNumber.StartsWith(rule.StartNumber.ToString()) && 
+                                                                        creditCardNumber.Length == rule.Length);
+
+            return (matchedRule != null) ? matchedRule.CardType : CardType.Unknown;
         }
     }
 }
