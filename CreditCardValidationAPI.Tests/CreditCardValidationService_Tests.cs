0127a27d277dc5358143a69a4cd5c00a3085e8c5
Sarawut Positwinyu <sarawut.positwinyu@gmail.com>
2018-07-10 21:40:26 +0700

    Add CreditCard Service Validation Test

---
+++ CreditCardValidationAPI.Tests/CreditCardValidationService_Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CreditCardValidationAPI.Tests
+{
+    public class CreditCardValidationService_Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
